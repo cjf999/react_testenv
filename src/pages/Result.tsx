28294631914e9e1ad0a0cdc83dae2db3a332dff7
FinnAmini <finnamini2@gmail.com>
2026-02-12 21:24:26 +0100

    :sparkles: Add api communication

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,9 +1,34 @@
+import { useEffect, useState, useMemo } from "react";
 import { endTime, startTime } from "hono/timing";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-export default function Result(){
+export default function Result() {
+
+    const { state } = useLocation();
+    const [simulationId, setSimulationId] = useState("");
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        setSimulationId(state?.simId || "");
+    }, [state])
+
+    useEffect(() => {
+        fetchData(simulationId);
+    }, [simulationId])
+
+    const fetchData = (simId: string | null) => {
+        if (!simId) return;
+        console.log("fetching", "https://gems.hciuse.sh/simulations/" + simId)
+        fetch("https://gems.hciuse.sh/simulations/" + simId).then(async res => {
+            setData(await res.json())
+        });
+    }
+
+    useEffect(() => {
+        console.log('data:', data);
+    }, [data])
 
     const inputParams = [{
         id: "1337420"
@@ -20,14 +45,30 @@ export default function Result(){
         errorMessage: "kein fehler aufgetreten"
     }]
 
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const data = new FormData(e.currentTarget);
+        const simId = data.get("id") as string;
+        console.log('looking for simid', simId);
+        setSimulationId(simId);
+    }
+
     return(
         <>
         <Navbar />
         <h1>Result View</h1>
         <h2>Hier können durchgeführte Simulationen über ihre id geladen werden.</h2>
         gib hier die id der simulation ein, um die ergebnisse zu laden:
-        beispiel: {inputParams[0].id}
-        <input type="text" />
+        aktuelle simulation: {simulationId}
+        <form onSubmit={handleSubmit} className="result-form">
+            <div>
+                {
+                    simulationId
+                    ? <input name="id" type="text" value={simulationId} />
+                    : <input name="id" type="text" />
+                }
+            </div>
+        </form>
         <Link to={"/"}>Zurück</Link>
         <Footer />
         </>
--- src/pages/Setup.tsx
+++ src/pages/Setup.tsx
@@ -1,9 +1,11 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 
 function Setup(){
+
+    const navigate = useNavigate();
 
     //sim parameters
     // var simID = 1337;
@@ -41,95 +43,142 @@ function Setup(){
     }
     */
 
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const data = new FormData(e.currentTarget);
+        const values = Object.fromEntries(data.entries());
+
+        const payload = {
+            "populationSize": Number(values.popSize),
+            "transmissionRate": Number(values.transmissionRate),
+            "avgHouseholdSize": Number(values.avgHouseholdSize),
+            "avgOfficeSize": Number(values.avgOfficeSize),
+            "avgSchoolSize": Number(values.avgSchoolSize),
+            "batchRuns": 1,
+            "interventions": [
+                {
+                    "type": values.intervention,
+                    "trigger": "symptom",
+                    "duration": 14
+                }
+            ],
+            "startDate": values.startDate,
+            "endDate": values.endDate,
+            "seed": Number(values.seed)
+        }
+
+        try {
+            const res = await fetch("https://gems.hciuse.sh/simulations", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(payload)
+            });
+
+            if (res.ok) {
+                const result = await res.json();
+                console.log("Created:", result);
+                navigate("/result", { state: { simId: result.simulationId } });
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
+
+        
+    }
+
     return(
         <>
         <Navbar />
             <h2 style={{paddingLeft: "50px", marginTop: "35px"}}>Nachfolgend die Parameter eingeben:</h2>
-            <div className="input-rows">
-                <div className="setup-row">
-                    <div className="param">
-                        <p>Hier wird die  <i>Populationsgröße</i> eingegeben</p>
-                        <input type="text" />
-                        <div>{params.map((item) => (
-                            <p style={{fontWeight: "800"}}>Der Placeholder-Wert ist: {item.popSize}</p>
-                        )
-                        )}</div>
+            <form className="setup-form" onSubmit={handleSubmit}>
+                <div className="input-rows">
+                    <div className="setup-row">
+                        <div className="param">
+                            <p>Hier wird die  <i>Populationsgröße</i> eingegeben</p>
+                            <input name="popSize" type="number" />
+                            <div>{params.map((item) => (
+                                <p style={{fontWeight: "800"}}>Der Placeholder-Wert ist: {item.popSize}</p>
+                            )
+                            )}</div>
+                        </div>
+                        <div className="param">
+                            <p>Hier wird die <i>Übertragungsrate</i> angegeben</p>
+                            <input name="transmissionRate" type="number" step="0.01" min="0" />
+                            <div>{params.map((item) => (
+                                <p style={{fontWeight: "800"}}>Der Placeholder-Wert ist: {item.transmissionRate}</p>
+                            )
+                            )}</div>
+                        </div>
+                        <div className="param">
+                            <p>Hier wird die <i>Intervention</i> angegeben</p>
+                            <input name="intervention" type="text" />
+                            <div>{params.map((item) => (
+                                <p style={{fontWeight: "800"}}>Der Placeholder-Wert ist: {item.intervention}</p>
+                            )
+                            )}</div>                        
+                        </div>
                     </div>
-                    <div className="param">
-                        <p>Hier wird die <i>Übertragungsrate</i> angegeben</p>
-                        <input type="text" />
-                        <div>{params.map((item) => (
-                            <p style={{fontWeight: "800"}}>Der Placeholder-Wert ist: {item.transmissionRate}</p>
-                        )
-                        )}</div>
+                    {/** second row */}
+                    <div className="setup-row">
+                        <div className="param">
+                            <p>Hier wird die <i>Household Size</i> angegeben</p>
+                            <input name="avgHouseholdSize" type="number" />
+                            <div>{params.map((item) => (
+                                <p style={{fontWeight: "800"}}>Der Placeholder-Wert ist: {item.avgHouseholdSize}</p>
+                            )
+                            )}</div>
+                        </div>
+                        <div className="param">
+                            <p>Hier wird die <i>School Size</i> angegeben</p>
+                            <input name="avgSchoolSize" type="number" />
+                            <div>{params.map((item) => (
+                                <p style={{fontWeight: "800"}}>Der Placeholder-Wert ist: {item.avgSchoolSize}</p>
+                            )
+                            )}</div>                        
+                        </div>
+                        <div className="param">
+                            <p>Hier wird die <i>Office Size</i> angegeben</p>
+                            <input name="avgOfficeSize" type="number" />
+                            <div>{params.map((item) => (
+                                <p style={{fontWeight: "800"}}>Der Placeholder-Wert ist: {item.avgOfficeSize}</p>
+                            )
+                            )}</div>                        
+                        </div>
                     </div>
-                    <div className="param">
-                        <p>Hier wird die <i>Intervention</i> angegeben</p>
-                        <input type="text" />
-                        <div>{params.map((item) => (
-                            <p style={{fontWeight: "800"}}>Der Placeholder-Wert ist: {item.intervention}</p>
-                        )
-                        )}</div>                        
+                    {/** third row */}
+                    <div className="setup-row">
+                        <div className="param">
+                            <p>Hier wird das <i>Startdatum</i> angegeben</p>
+                            <input name="startDate" type="date" />
+                            <div>{params.map((item) => (
+                                <p style={{fontWeight: "800"}}>Der Placeholder-Wert ist: {item.startDate}</p>
+                            )
+                            )}</div>                        
+                        </div>
+                        <div className="param">
+                            <p>Hier wird das <i>Enddatum</i> angegeben</p>
+                            <input name="endDate" type="date" />
+                            <div>{params.map((item) => (
+                                <p style={{fontWeight: "800"}}>Der Placeholder-Wert ist: {item.endDate}</p>
+                            )
+                            )}</div>                        
+                        </div>
+                        <div className="param">
+                            <p>Hier wird der <i>Seed</i> angegeben</p>
+                            <input name="seed" type="number" />
+                            <div>{params.map((item) => (
+                                <p style={{fontWeight: "800"}}>Der Placeholder-Wert ist: {item.seed}</p>
+                            )
+                            )}</div>                        
+                        </div>
                     </div>
-                </div>
-                {/** second row */}
-                 <div className="setup-row">
-                    <div className="param">
-                        <p>Hier wird die <i>Household Size</i> angegeben</p>
-                        <input type="text" />
-                        <div>{params.map((item) => (
-                            <p style={{fontWeight: "800"}}>Der Placeholder-Wert ist: {item.avgHouseholdSize}</p>
-                        )
-                        )}</div>
-                    </div>
-                    <div className="param">
-                        <p>Hier wird die <i>School Size</i> angegeben</p>
-                        <input type="text" />
-                        <div>{params.map((item) => (
-                            <p style={{fontWeight: "800"}}>Der Placeholder-Wert ist: {item.avgSchoolSize}</p>
-                        )
-                        )}</div>                        
-                    </div>
-                    <div className="param">
-                        <p>Hier wird die <i>Office Size</i> angegeben</p>
-                        <input type="text" />
-                        <div>{params.map((item) => (
-                            <p style={{fontWeight: "800"}}>Der Placeholder-Wert ist: {item.avgOfficeSize}</p>
-                        )
-                        )}</div>                        
-                    </div>
-                </div>
-                {/** third row */}
-                <div className="setup-row">
-                    <div className="param">
-                        <p>Hier wird das <i>Startdatum</i> angegeben</p>
-                        <input type="text" />
-                        <div>{params.map((item) => (
-                            <p style={{fontWeight: "800"}}>Der Placeholder-Wert ist: {item.startDate}</p>
-                        )
-                        )}</div>                        
-                    </div>
-                    <div className="param">
-                        <p>Hier wird das <i>Enddatum</i> angegeben</p>
-                        <input type="text" />
-                        <div>{params.map((item) => (
-                            <p style={{fontWeight: "800"}}>Der Placeholder-Wert ist: {item.endDate}</p>
-                        )
-                        )}</div>                        
-                    </div>
-                    <div className="param">
-                        <p>Hier wird der <i>Seed</i> angegeben</p>
-                        <input type="text" />
-                        <div>{params.map((item) => (
-                            <p style={{fontWeight: "800"}}>Der Placeholder-Wert ist: {item.seed}</p>
-                        )
-                        )}</div>                        
-                    </div>
-                </div>
-                <div className="button-container">
-                    <Link to={"/received"}  className="start-button">Sim starten</Link>
-                </div>                 
-            </div>       
+                    <button type="submit" className="button-container start-button" >
+                        Marvin stinkt 💩
+                    </button>                 
+                </div>       
+            </form>
             <Footer />        
         </>
     )
