6df32341793b144a43b203f70a06003c0e483879
Carl Fedrowitz <carl.fedrowitz@student.uni-luebeck.de>
2026-02-12 16:24:11 +0100

    deleted more deprecated files

--- src/components/Card.tsx
+++
@@ -1,9 +0,0 @@
-function Card(){
-    return(
-        <div className="card">
-            this is some testcontent inside of a card element
-        </div>
-    )
-}
-
-export default Card
--- src/components/ParamInput.tsx
+++
@@ -1,5 +0,0 @@
-export default function Inputfield(){
-    return(
-        <input></input>
-    )
-}
